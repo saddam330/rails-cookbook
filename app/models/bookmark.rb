575feb8cf541fdcd1930d88a6eda04e14e29aa05
Saddam Hussain saddam5533@hotmail.com <saddam5533@hotmail.com>
2025-05-12 18:04:00 +0100

    backend is fully working now

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
   validates :recipe_id, uniqueness: { scope: :category_id }
-  validates :coment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 6 }
 end
